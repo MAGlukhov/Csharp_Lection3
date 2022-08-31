3ecdf674cb1f09e77a9786d228fe0b47a618d283
Maxim <Thickfod@gmail.com>
2022-08-31 15:22:36 +0300

    Lection3 Tasks

---
+++ For/Program.cs
@@ -0,0 +1,24 @@
+﻿// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for(int i = 0; i < count; i++)
+//     {
+//         result = result + text; 
+//     }
+//     return result;
+// }
+
+// Console.WriteLine(Method4(10, "Z "));
+
+// void MethodDoWhile(int count, string text)
+// {
+//     int i = 0;
+//     do
+//     {
+//         Console.Write(i + text + " ");
+//         i++;
+//     }
+//     while (i < count);
+// }
+
+// MethodDoWhile(10, "Z");
